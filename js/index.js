fb50faf2052cdc3307576d3cc152b22b32553404
zz1998022 <1074045407@qq.com>
2020-04-21 12:40:39 +0800

    添加:完成tab切换

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
     /* 轮播图部分 */
+    banner();
+    // tab切换
+    tabToggle($('.jieshao-tab>ul>li'), $('.jieshao-content>.jieshao-item'),'sel');
+})
+
+
+// 轮播图部分
+function banner() {
     // 获取小圆点
     let currentOl = $('.banner-main>li');
     // 根据图片数量创建小圆点
@@ -12,7 +20,7 @@ $(document).ready(function() {
     // 给第一个小圆点添加类名
     current.eq(0).addClass('active')
     // 绑定事件
-    current.on('click',function() {
+    current.on('click', function () {
         // 获取当前元素的索引
         let index = $(this).index();
         // 排他
@@ -24,4 +32,32 @@ $(document).ready(function() {
         // 让索引对应的图片显示出来
         currentOl.eq(index).stop().fadeIn();
     })
-})
+
+}
+
+/**
+ * tab切换
+ * @param {string} element 
+ * @param {string} child 
+ * @param {string} classname
+ */
+
+function tabToggle(element, child, classname) {
+    // 获取元素
+    let el = $(element);
+    let cld = $(child);
+
+    // 绑定事件
+    el.on('click', function () {
+        // 获取当前元素的索引
+        let index = $(this).index();
+        // 移出其他元素的类名
+        el.removeClass(classname);
+        // 给当前元素添加类名
+        $(this).addClass(classname);
+        // 让其他元素淡出
+        cld.stop().fadeOut();
+        // 让索引对应的元素淡入
+        cld.eq(index).fadeIn().css('display', 'flex');
+    })
+}
